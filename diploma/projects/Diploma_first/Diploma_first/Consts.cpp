a7f8e5e6c14bd9c8b333da1c5c286a632d2511f5
DmitriiSukhomlinov <sukhomlinovdmitrii@yandex.ru>
2018-04-01 12:09:44 +0700

    Exel

--- diploma/projects/Diploma_first/Diploma_first/Consts.cpp
+++ diploma/projects/Diploma_first/Diploma_first/Consts.cpp
@@ -8,8 +8,30 @@ static const char* sample1 = "samples/photo1.jpg";
 static const char* sample2 = "samples/photo2.jpg";
 static const char* sample3 = "samples/photo3.jpg";
 static const char* sample4 = "samples/photo4.jpg";
+
 static const char* sampleMe1 = "samples/Me/sampleMe1.jpg";
 static const char* sampleMe2 = "samples/Me/sampleMe2.jpg";
 static const char* sampleMe3 = "samples/Me/sampleMe3.jpg";
 static const char* sampleMe4 = "samples/Me/sampleMe4.jpg";
 
+static const char* sampleSame1 = "samples/faces/same_faces/1.jpg";
+static const char* sampleSame2 = "samples/faces/same_faces/2.jpg";
+static const char* sampleSame3 = "samples/faces/same_faces/3.jpg";
+static const char* sampleSame4 = "samples/faces/same_faces/4.jpg";
+static const char* sampleSame5 = "samples/faces/same_faces/5.jpg";
+static const char* sampleSame6 = "samples/faces/same_faces/6.jpg";
+static const char* sampleSame7 = "samples/faces/same_faces/7.jpg";
+static const char* sampleSame8 = "samples/faces/same_faces/8.jpg";
+static const char* sampleSame9 = "samples/faces/same_faces/9.jpg";
+static const char* sampleSame10 = "samples/faces/same_faces/10.jpg";
+
+static const char* sampleDiff1 = "samples/faces/different_faces/1.jpg";
+static const char* sampleDiff2 = "samples/faces/different_faces/2.jpg";
+static const char* sampleDiff3 = "samples/faces/different_faces/3.jpg";
+static const char* sampleDiff4 = "samples/faces/different_faces/4.jpg";
+static const char* sampleDiff5 = "samples/faces/different_faces/5.jpg";
+static const char* sampleDiff6 = "samples/faces/different_faces/6.jpg";
+static const char* sampleDiff7 = "samples/faces/different_faces/7.jpg";
+static const char* sampleDiff8 = "samples/faces/different_faces/8.jpg";
+static const char* sampleDiff9 = "samples/faces/different_faces/9.jpg";
+static const char* sampleDiff10 = "samples/faces/different_faces/10.jpg";
