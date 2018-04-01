78aec7adf9bbfb4e098f8cba3b911312695e9133
DmitriiSukhomlinov <sukhomlinovdmitrii@yandex.ru>
2018-04-01 22:48:07 +0700

    Exel 2

--- diploma/projects/Diploma_first/Diploma_first/Consts.cpp
+++ diploma/projects/Diploma_first/Diploma_first/Consts.cpp
@@ -1,3 +1,5 @@
+#include <array>
+
 static const char* key = "OGObpkvXCrfHElL9WEBXFpuQJKKAHYH7x3RhCCx5pPIlC3Dhs3uZ6IcSZOdbaGZ85xAyP2Y13Lvh+UYBdMnXdnPHD7Nox08lGz7CaKye8Kle+AK7evFqLUkSPOH83PG4FTYqAKnw5SdinVo0SK1yng0PiJKCpYOjPK2qNEqKPAE=";
 
 //The path to the facesdk.dll
@@ -14,24 +16,24 @@ static const char* sampleMe2 = "samples/Me/sampleMe2.jpg";
 static const char* sampleMe3 = "samples/Me/sampleMe3.jpg";
 static const char* sampleMe4 = "samples/Me/sampleMe4.jpg";
 
-static const char* sampleSame1 = "samples/faces/same_faces/1.jpg";
-static const char* sampleSame2 = "samples/faces/same_faces/2.jpg";
-static const char* sampleSame3 = "samples/faces/same_faces/3.jpg";
-static const char* sampleSame4 = "samples/faces/same_faces/4.jpg";
-static const char* sampleSame5 = "samples/faces/same_faces/5.jpg";
-static const char* sampleSame6 = "samples/faces/same_faces/6.jpg";
-static const char* sampleSame7 = "samples/faces/same_faces/7.jpg";
-static const char* sampleSame8 = "samples/faces/same_faces/8.jpg";
-static const char* sampleSame9 = "samples/faces/same_faces/9.jpg";
-static const char* sampleSame10 = "samples/faces/same_faces/10.jpg";
+static std::array<const char*, 10> sampleSame = { "samples/faces/same_faces/1.jpg" ,
+                                                  "samples/faces/same_faces/2.jpg" ,
+                                                  "samples/faces/same_faces/3.jpg" ,
+                                                  "samples/faces/same_faces/4.jpg" ,
+                                                  "samples/faces/same_faces/5.jpg" ,
+                                                  "samples/faces/same_faces/6.jpg" ,
+                                                  "samples/faces/same_faces/7.jpg" ,
+                                                  "samples/faces/same_faces/8.jpg" ,
+                                                  "samples/faces/same_faces/9.jpg" ,
+                                                  "samples/faces/same_faces/10.jpg" };
 
-static const char* sampleDiff1 = "samples/faces/different_faces/1.jpg";
-static const char* sampleDiff2 = "samples/faces/different_faces/2.jpg";
-static const char* sampleDiff3 = "samples/faces/different_faces/3.jpg";
-static const char* sampleDiff4 = "samples/faces/different_faces/4.jpg";
-static const char* sampleDiff5 = "samples/faces/different_faces/5.jpg";
-static const char* sampleDiff6 = "samples/faces/different_faces/6.jpg";
-static const char* sampleDiff7 = "samples/faces/different_faces/7.jpg";
-static const char* sampleDiff8 = "samples/faces/different_faces/8.jpg";
-static const char* sampleDiff9 = "samples/faces/different_faces/9.jpg";
-static const char* sampleDiff10 = "samples/faces/different_faces/10.jpg";
+static std::array<const char*, 10> sampleDiff = { "samples/faces/different_faces/1.jpg" ,
+                                                  "samples/faces/different_faces/2.jpg" ,
+                                                  "samples/faces/different_faces/3.jpg" ,
+                                                  "samples/faces/different_faces/4.jpg" ,
+                                                  "samples/faces/different_faces/5.jpg" ,
+                                                  "samples/faces/different_faces/6.jpg" ,
+                                                  "samples/faces/different_faces/7.jpg" ,
+                                                  "samples/faces/different_faces/8.jpg" ,
+                                                  "samples/faces/different_faces/9.jpg" ,
+                                                  "samples/faces/different_faces/10.jpg" };
--- diploma/projects/Diploma_first/Diploma_first/Main.cpp
+++ diploma/projects/Diploma_first/Diploma_first/Main.cpp
@@ -23,32 +23,69 @@ int main() {
     CHECK_RETURN(result == FSDKE_OK, "Correct initialization of the dll",
                                      "Initialization error", 0);
 
-    result = Settings::setFaceDetectionParametrs();
+    //result = Settings::setFaceDetectionParametrs();
+    //CHECK_PAUSE(result == FSDKE_OK, "The parametrs were setted correctly",
+    //                                "Error in the face detection parametrs");
+    //
+    //result = Settings::setFaceDetectionThreshold();
+    //CHECK_PAUSE(result == FSDKE_OK, "The threshold was setted correctly",
+    //                                "Face detection threshold wasn't setted");
+
+    result = FSDK_SetFaceDetectionParameters(false, false, 384);
     CHECK_PAUSE(result == FSDKE_OK, "The parametrs were setted correctly",
                                     "Error in the face detection parametrs");
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = i; j < 10; j++) {
+            //Pointer to the library performance of the sample picture
+            shared_ptr<HImage> image1(new HImage);
+            //Creatiing the library performance of the sample picture
+            result = FSDK_LoadImageFromFile(image1.get(), sampleSame[i]);
+            //CHECK_RETURN(result == FSDKE_OK, "The sample was successfully loaded",
+            //    "Sample loading error", 0);
+            //CHECK_RETURN(image1 != nullptr, "Image pointer was created sucsessfully",
+            //    "Unexpected behavior, image pointer is null",
+            //    0);
+
+            shared_ptr<HImage> image2(new HImage);
+            //Creatiing the library performance of the sample picture
+            result = FSDK_LoadImageFromFile(image2.get(), sampleSame[j]);
+            //CHECK_RETURN(result == FSDKE_OK, "The sample was successfully loaded",
+            //    "Sample lading error", 0);
+            //CHECK_RETURN(image2 != nullptr, "Image pointer was created sucsessfully",
+            //    "Unexpected behavior, image pointer is null",
+            //    0);
+
+
+            //Library performance of the face
+            shared_ptr<FSDK_FaceTemplate> faceTemplate1(new FSDK_FaceTemplate);
+            result = FSDK_GetFaceTemplate(*image1, faceTemplate1.get());
+            //CHECK_RETURN(result == FSDKE_OK, "The Face template was successfully defined",
+            //    "Face template definding error", 0);
+            //CHECK_RETURN(faceTemplate1 != nullptr, "Face template pointer was created sucsessfully",
+            //    "Unexpected behavior, face template pointer is null",
+            //    0);
+
+            shared_ptr<FSDK_FaceTemplate> faceTemplate2(new FSDK_FaceTemplate);
+            result = FSDK_GetFaceTemplate(*image2, faceTemplate2.get());
+            //CHECK_RETURN(result == FSDKE_OK, "The Face template was successfully defined",
+            //    "Face template definding error", 0);
+            //CHECK_RETURN(faceTemplate2 != nullptr, "Face template pointer was created sucsessfully",
+            //    "Unexpected behavior, face template pointer is null",
+            //    0);
+
+            float similarity = -1.0;
+            result = FSDK_MatchFaces(faceTemplate1.get(), faceTemplate2.get(), &similarity);
+            //CHECK_RETURN(result == FSDKE_OK, "Face mutch operation successfull",
+            //                                 "Face mutch operation error", 0);
+            
+            //if (similarity > 0.4 && i != j) {
+                cout << "Similarity of " << i + 1 << " and " << j + 1 << " is " << similarity << endl;
+            //}
+        }
+        cout << endl << endl;
+    }
     
-    result = Settings::setFaceDetectionThreshold();
-    CHECK_PAUSE(result == FSDKE_OK, "The threshold was setted correctly",
-                                    "Face detection threshold wasn't setted");
-
-    //Pointer to the library performance of the sample picture
-    shared_ptr<HImage> image1(new HImage);
-    //Creatiing the library performance of the sample picture
-    result = FSDK_LoadImageFromFile(image1.get(), sampleMe3);
-    CHECK_RETURN(result == FSDKE_OK, "The sample was successfully loaded",
-                                     "Sample loading error", 0);
-    CHECK_RETURN(image1 != nullptr, "Image pointer was created sucsessfully",
-                                   "Unexpected behavior, image pointer is null",
-                                   0);
-
-    shared_ptr<HImage> image2(new HImage);
-    //Creatiing the library performance of the sample picture
-    result = FSDK_LoadImageFromFile(image2.get(), sampleMe4);
-    CHECK_RETURN(result == FSDKE_OK, "The sample was successfully loaded",
-                                     "Sample loading error", 0);
-    CHECK_RETURN(image2 != nullptr, "Image pointer was created sucsessfully",
-                                    "Unexpected behavior, image pointer is null",
-                                    0);
 
 /*
     //The struct represent 70 points of the face
@@ -71,32 +108,15 @@ int main() {
                                           "Unexpected behavior, Face position pointer is null",
                                           0); */
 
-    //Library performance of the face
-    shared_ptr<FSDK_FaceTemplate> faceTemplate1(new FSDK_FaceTemplate);
-    result = FSDK_GetFaceTemplate(*image1, faceTemplate1.get());
-    CHECK_RETURN(result == FSDKE_OK, "The Face template was successfully defined",
-                                     "Face template definding error", 0);
-    CHECK_RETURN(faceTemplate1 != nullptr, "Face template pointer was created sucsessfully",
-                                    "Unexpected behavior, face template pointer is null",
-                                    0);
-
-    shared_ptr<FSDK_FaceTemplate> faceTemplate2(new FSDK_FaceTemplate);
-    result = FSDK_GetFaceTemplate(*image2, faceTemplate2.get());
-    CHECK_RETURN(result == FSDKE_OK, "The Face template was successfully defined",
-                                     "Face template definding error", 0);
-    CHECK_RETURN(faceTemplate2 != nullptr, "Face template pointer was created sucsessfully",
-                                    "Unexpected behavior, face template pointer is null",
-                                    0);
 
     //The similarity of two represented faces
-    float similarity = -1.0;
-    time.startTimer("Face match");
-    result = FSDK_MatchFaces(faceTemplate1.get(), faceTemplate2.get(), &similarity);
-    time.checkTimer(TimeChecker::Microseconds);
-    CHECK_RETURN(result == FSDKE_OK, "Face mutch operation successfull",
-                                     "Face mutch operation error", 0);
-
-    cout << "Similarity is " << (int)(similarity * 100) << "%" << endl;
+    //float similarity = -1.0;
+    //time.startTimer("Face match");
+    //result = FSDK_MatchFaces(faceTemplate1.get(), faceTemplate2.get(), &similarity);
+    //time.checkTimer(TimeChecker::Microseconds);
+    //CHECK_RETURN(result == FSDKE_OK, "Face mutch operation successfull",
+    //                                 "Face mutch operation error", 0);
+    //cout << "Similarity is " << (int)(similarity * 100) << "%" << endl;
     
 
 
