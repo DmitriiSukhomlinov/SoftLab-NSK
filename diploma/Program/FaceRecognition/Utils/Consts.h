a2351dd983a693debcf781eafb6611c9127b4bfa
DmitriiSukhomlinov <sukhomlinovdmitrii@yandex.ru>
2020-03-19 10:44:45 +0700

    check

--- diploma/Program/FaceRecognition/Utils/Consts.h
+++ diploma/Program/FaceRecognition/Utils/Consts.h
@@ -3,5 +3,5 @@
 static const char* key = "RymX9UbV75nb/1dOsFppHvgHL+77pHfjeb0GShSqxn3yjlca3d0uvSpS80boEnyC9XwMWCaYEdh9zNySHzMnq8ig11xQe71AGWrlTQy/nE69EjMr8xiAFhuLUF+8lyhayzxTGFgr8npnDT7wU9N6rdMvHWIS0dWe6qz9mf7BY6s=";
 
 //The path to the facesdk.dll
-//Null pointer means the current directory
+//Null pointer means the current directory123
 static char* dllPath = nullptr;
